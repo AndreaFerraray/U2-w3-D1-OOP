db2ce9ee32096b0a7feeca122a889f9c720621c2
AndreaFerrara13 <134604849+AndreaFerrara13@users.noreply.github.com>
2023-08-21 15:16:12 +0200

    OOP

---
+++ class.js
@@ -0,0 +1,39 @@
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+  static isOlder(Users) {
+    return User.age > User.age;
+  }
+}
+const Users = [
+  new User("Gino", "Pinguino", "32", "Roma"),
+  // console.log(ginoPinguino);
+  new User("Marco", "Pilo", "18", "Milano"),
+  new User("fabrizio", "Turo", "28", "Roma"),
+  new User("Giulia", "Cafro", "32", "Napoli"),
+];
+console.log(User.isOlder());
+
+// ----------------esercizio 2-----------------------
+
+class Pet {
+  constructor(firstName, lastName, age, location) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+  static ownerName(Pet) {
+    if { pet.ownerName === Pet.ownerName;
+        return true }
+  }
+}
+const pets=[
+    new Pet('ciccio', 'graziano','cane', 'belga')
+    new Pet('dari', 'graziano', 'cane', 'alano',)
+    nwe Pet('frew','Tiziana' ,'coniglio', 'nn')
+]
